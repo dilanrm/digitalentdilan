65976d795101477d014fcb452e7a8ecd49fdf0bd
dilanrm <43987073+dilanrm@users.noreply.github.com>
2018-11-12 14:20:28 +0700

    Add files via upload

---
+++ digitalentdilan/footer.php
@@ -0,0 +1,14 @@
+	</div>
+<footer class="site-footer" style="padding:10px">
+            <div class="row">
+                <div class="col-sm-6">
+                    <h5>Kuunibyou page © 2017</h5></div>
+                <div class="col-sm-6 social-icons"><a href="#"><i class="fa fa-linkedin-square"></i></a><a href="#"><i class="fa fa-github"></i></a><a href="#"><i class="fa fa-at"></i></a></div>
+            </div>
+    
+    </footer>
+    <script src="assets/js/jquery.min.js"></script>
+    <script src="assets/bootstrap/js/bootstrap.min.js"></script>
+</body>
+
+</html>
