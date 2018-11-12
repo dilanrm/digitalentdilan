26c39d5cf922e9dde3a1a37c3f53b1f2abaa0b26
dilanrm <43987073+dilanrm@users.noreply.github.com>
2018-11-12 14:17:49 +0700

    Add files via upload

---
+++ digitalentdilan/header.php
@@ -0,0 +1,11 @@
+<?php include 'connect.php';?>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/fonts/font-awesome.min.css">
+    <link rel="stylesheet" href="assets/css/user.css">
+	<link rel="icon" type="image/gif" href="assets/img/icon.png" />
+
+
